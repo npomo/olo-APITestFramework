e7fa051e37d0b09e4194902b335b570fca758e42
Nick Pomo <nicholas.pomo@gmail.com>
2020-08-13 00:24:37 -0400

    Project Scaffolding & Tooling
    - Created the base Unit test project in VS2019
    - Installed RestSharp and other supporting packages
    - Wrote 1 test to orient myself with RestSharp

---
+++ Olo-APITestFramework/Olo-APITestFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Olo-APITestFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Olo-APITestFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb4cf449-11bf-413e-9d68-1db2bb9e7487")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Olo-APITestFramework/Olo-APITestFramework/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Olo_APITestFramework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            RestClient client = new RestClient("https://jsonplaceholder.typicode.com/");
+
+            RestRequest request2 = new RestRequest("posts/1", Method.GET);
+            var response2 = await client.ExecuteGetAsync(request2);
+            
+            RestRequest request = new RestRequest("posts/1", Method.GET);
+            var response = await client.ExecuteGetAsync(request);
+
+            await Console.Out.WriteLineAsync(response2.Content + " |||| " + response.Content);
+
+        }
+    }
+}
