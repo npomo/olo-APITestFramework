742e1767cc2861c2c760ca42f3ce23a7dc49f500
Nick Pomo <nicholas.pomo@gmail.com>
2020-08-13 23:23:35 -0400

    Install FluentAssertions and Test

---
+++ Olo-APITestFramework/Olo-APITestFramework/JSONPlaceHolder_GETPostsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Olo_APITestFramework
+{
+    [TestClass]
+    public class JSONPlaceHolder_GETPostsTests
+    {
+        [TestMethod]
+        public void GetAllPosts_ShouldReturn200()
+        {
+            RestClient client = new RestClient("https://jsonplaceholder.typicode.com/");
+            RestRequest request = new RestRequest("posts/1");
+            var response = client.Get(request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK, "because the Get call was successful");
+     
+
+        }
+    }
+}
