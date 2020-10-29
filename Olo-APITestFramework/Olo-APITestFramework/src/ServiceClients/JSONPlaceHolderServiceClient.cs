53571e73023c63913adfb460b1926cd41df81d4b
Nick Pomo <nicholas.pomo@olo.com>
2020-10-28 22:28:14 -0400

    squash me commit + the newer commit
    squash into this guy

--- Olo-APITestFramework/Olo-APITestFramework/src/ServiceClients/JSONPlaceHolderServiceClient.cs
+++ Olo-APITestFramework/Olo-APITestFramework/src/ServiceClients/JSONPlaceHolderServiceClient.cs
@@ -43,6 +43,8 @@ namespace Olo_APITestFramework.src.ServiceClients
             };
         }
         //another commit to practice with
+        //yet another commit to practice with
+        //another commit
 
         public async Task<JSONPlaceHolderGetOneResponse> GetOnePost(string postId)
         {
